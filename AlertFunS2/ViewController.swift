f048d35b780441510ad7df2ff8837075c060457b
Gina Sprint <sprint@gonzaga.edu>
2020-09-28 11:39:57 -0700

    Initial Commit

---
+++ AlertFunS2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertFunS2
+//
+//  Created by Gina Sprint on 9/28/20.
+//  Copyright © 2020 Gina Sprint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
