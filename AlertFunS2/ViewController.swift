f4c82423e36ce0507b7abf916b7a3257590b321c
Gina Sprint <sprint@gonzaga.edu>
2020-09-28 12:27:19 -0700

    Initial

--- AlertFunS2/ViewController.swift
+++ AlertFunS2/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // task: in IB, add a UITextField that is as wide as the iPhone SE
+    // ignore the layout warnings....
+    // add a button below that says "Echo Back"
+    // wire up the connections for an IBOutlet and an IBAction in ViewController
+    // print out a log message proving your IBAction is set up
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
